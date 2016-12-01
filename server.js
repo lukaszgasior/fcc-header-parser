d5e8c6416ae2f95d93bccaaa08a473ef87f3c7b9
Lukasz Gasior <lukaszgasior@outlook.com>
2016-12-01 18:59:08 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+app.get('/',function(req, res) {
+    res.send('Hello World!');
+})
+
+app.listen(PORT, function(argument) {
+    console.log('Server listening on port ' + PORT);
+});
